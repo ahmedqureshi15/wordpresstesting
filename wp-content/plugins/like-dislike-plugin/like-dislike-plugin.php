0b32af29807959a2fb55f0700db7f8e77a1c9913
Umair Qureshi <umairqureshi_6@hotmail.com>
2019-02-18 14:00:50 +0000

    modified plugin `Like Dislike Plugin` version 1.0 (after deactivation of `My First Plugin` version 1.0)

---
+++ wp-content/plugins/like-dislike-plugin/like-dislike-plugin.php
@@ -0,0 +1,10 @@
+
+<?php
+/**
+ * Plugin Name: Like Dislike Plugin
+ * Plugin URI: http://www.mywebsite.com/like-dislike-plugin
+ * Description: The plugin is doing like or dislike.
+ * Version: 1.0
+ * Author: Muhammad Ahmed Qureshi
+ * Author URI: http://www.mywebsite.com
+ */
