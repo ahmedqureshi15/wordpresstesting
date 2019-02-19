f8812f131b7ef355f8e7899418909ea244e4ab1d
Umair Qureshi <umairqureshi_6@hotmail.com>
2019-02-19 04:40:28 +0000

    modified plugin `First Plugin` version 1.0 (after activation of `First Plugin` version 1.0)

---
+++ wp-content/plugins/first-plugin/first-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: First Plugin
+* Plugin URI: https://wordpress-162778-721634.cloudwaysapps.com/
+* Description: This is the very first plugin I ever created.
+* Version: 1.0
+* Author: Masroor
+* Author URI: https://wordpress-162778-721634.cloudwaysapps.com/
+**/
