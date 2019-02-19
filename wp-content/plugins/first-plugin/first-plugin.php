b5769666cfc069c335fc62ca6808bc6e72e3d1fe
Umair Qureshi <umairqureshi_6@hotmail.com>
2019-02-19 04:43:43 +0000

    deleted plugin `First Plugin` version 1.0

--- wp-content/plugins/first-plugin/first-plugin.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* Plugin Name: First Plugin
-* Plugin URI: https://wordpress-162778-721634.cloudwaysapps.com/
-* Description: This is the very first plugin I ever created.
-* Version: 1.0
-* Author: Masroor
-* Author URI: https://wordpress-162778-721634.cloudwaysapps.com/
-**/
